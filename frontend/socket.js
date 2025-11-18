acbf220534799875587aecdde936e390097edb27
deblina00 <roydeblinasec@gmail.com>
2025-11-18 10:55:53 +0530

    socket url fix

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,5 +1,19 @@
+// import { io } from "socket.io-client";
+
+// const socket = io("ws://localhost:4000");
+
+// export default socket;
+
+
+// socket.js
 import { io } from "socket.io-client";
 
-const socket = io("ws://localhost:4000");
+const SOCKET_URL =
+  import.meta.env.VITE_API_BASE_URL?.replace("/api", "") ||
+  "http://localhost:4000"; // fallback for dev
+
+const socket = io(SOCKET_URL, {
+  transports: ["websocket", "polling"],
+});
 
 export default socket;
